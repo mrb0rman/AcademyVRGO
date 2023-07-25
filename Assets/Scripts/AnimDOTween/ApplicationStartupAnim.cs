431f1cd3dde89ae92261f98443b6595a9e45df1a
Keinih Danil <keinihd@gmail.com>
2023-07-25 09:37:29 +0300

    qwe

--- Assets/Scripts/AnimDOTween/ApplicationStartupAnim.cs
+++ Assets/Scripts/AnimDOTween/ApplicationStartupAnim.cs
@@ -1,14 +1,16 @@
 ﻿using System;
+using Script.Game;
 using UnityEngine;
 
 namespace Script
 {
     public class ApplicationStartupAnim : MonoBehaviour
     {
-        
+        public Camera camera;
         private void Start()
         {
-            
+            camera = Instantiate(Resources.Load<Camera>("Main Camera"));
+            Light directionalLight = Instantiate(Resources.Load<Light>("Directional Light"));
         }
     }
 }
---
+++ Assets/Scripts/Project/ApplicationStartup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Script.Project
+{
+    public class ApplicationStartup : MonoBehaviour
+    {
+        public Player player;
+        private void Start()
+        {
+            
+        }
+    }
+}
